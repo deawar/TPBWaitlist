5642de2fecb3b364c36c21f4d69ee2034ce1dd12
deawar <deawar@gmail.com>
2021-12-17 07:04:53 -0500

    updated npm versions of some modules

--- models/User.js
+++ models/User.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const uniqueValidator = require('mongoose-unique-validator');
+//const uniqueValidator = require('mongoose-unique-validator');
 
 // Email Validator fx
 const validateEmail = function (email) {
