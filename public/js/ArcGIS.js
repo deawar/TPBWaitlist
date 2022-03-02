c5cb8201eabeb06ec57e97215b83f2aea14e4373
deawar <deawar@gmail.com>
2022-03-01 21:26:20 -0500

    trying to catchup

--- public/js/ArcGIS.js
+++ public/js/ArcGIS.js
@@ -1,3 +1,4 @@
+// const apiKey = process.env.ArcGis_API_Key
 const apiKey = "AAPKb4a6ea84516a4efba14c236d8f3177fehN7FDSaoVfKQTpy_yHeUIZylKtG_AZNOX623vQN3jJaWAQuNlrFUk5ivmHUo1jbO";
 const basemapEnum = "ArcGIS:Navigation";
 
--- routes/maps.js
+++ routes/maps.js
@@ -4,14 +4,28 @@ const passport = require('passport');
 const mongoose = require('mongoose');
 const { ensureAuthenticated, forwardAuthenticated } = require('../config/auth');
 const os = require('os');
+
+const esriConfig = import("@arcgis/core/config.js");
+//const esriConfig = require('esriConfig');
+esriConfig.assetsPath = "node_modules/@arcgis/core/assets"; // relative to when running in root
 const PORT = process.env.PORT
+const mapAPIKey = process.env.ArcGis_API_Key
+const basemapEnum = "ArcGIS:Navigation";
 
 // To Load Host the app is working on
 const hostname = os.hostname();
 
 
 // Maps Page  ensureAuthenticated, 
-router.get('/', ensureAuthenticated, (req, res) =>
+router.get('/', ensureAuthenticated, (req, res) => {
+    const map = L.map('map',  {
+        minZoom: 2
+    }).setView([33.82017702602581, -83.84221905330062], 13);
+
+    L.esri.Vector.vectorBasemapLayer(basemapEnum, {
+        apiKey: mapAPIKey
+    }).addTo(map);
+
     res.render('map', {
     user: req.user
     })
@@ -22,6 +36,6 @@ router.get('/', ensureAuthenticated, (req, res) =>
             error: err
         });
     })
-);
+});
 
 module.exports = router;
